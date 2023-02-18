5031af8bee015c89843f0e668bbd886b55a6ed78
jordi <jordivallsplasmx@gmail.com>
2023-02-18 15:52:28 +0100

    Second commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let colores = [amarillo, rojo, verde, azul];
+
+console.log(colores);
+
+function jugar(){
+    colores[0].style.filter = "brightness(300%)";
+};
+
+
+console.log( colores[0].style.filter = "brightness(300%)");
+
+colores[0].style.filter = "brightness(300%)"
