ab6cb479f447e151505fdf57abb9fb5d87bbfec6
Jordi <jordivallsplasmx@gmail.com>
2023-02-20 10:45:30 +0100

    1.0

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
-let colores = [amarillo, rojo, verde, azul];
+let colores = [amarillo, verde, rojo, azul];
+let numeroNivel = 1;
+console.log(colores)    
 
-console.log(colores);
+    // colores[0].style.filter = 'brightness(200%)';    per a cambiar els colors 
+    // colores[1].style.filter = 'brightness(200%)';
+    // colores[2].style.filter = 'brightness(200%)';
+    // colores[3].style.filter = 'brightness(200%)';
 
-function jugar(){
-    colores[0].style.filter = "brightness(300%)";
-};
+function jugar() {
+
+    for (let i = 0; i < 1; i++) {
+        colores.sort( () => Math.random() - 0.5)
+    }
+    iluminar_carta()
+}
+
+function iluminar_carta() {
+
+    colores[0].style.filter = 'brightness(200%)'
+
+    setTimeout(() => {
+        colores[0].style.filter = 'brightness(100%)'
+        colores[1].style.filter = 'brightness(200%)'
+    }, 1000);
+
+    setTimeout(() => {
+        colores[1].style.filter = 'brightness(100%)'
+        colores[2].style.filter = 'brightness(200%)'
+    }, 2000);
+
+    setTimeout(() => {
+        colores[2].style.filter = 'brightness(100%)'
+        colores[3].style.filter = 'brightness(200%)'
+    }, 3000);
+
+    setTimeout(() => {
+        colores[3].style.filter = 'brightness(100%)'
+    }, 4000)
+}
 
 
-console.log( colores[0].style.filter = "brightness(300%)");
 
-colores[0].style.filter = "brightness(300%)"
+
+
