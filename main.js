e1883c2a0987ddb9e108cd6728303b4a9d4bde13
jordi <jordivallsplasmx@gmail.com>
2023-02-20 19:26:48 +0100

    Version 1.2

--- main.js
+++ main.js
@@ -43,36 +43,73 @@ function iluminar_carta() {
 
 function apretar_colors() {
     
-    amarillo.addEventListener('click', () => { 
-        orden.push(amarillo) 
-        console.log(orden)
+    colores[0].addEventListener('click', () => { 
+        orden.push(colores[0])
+        colores[0].style.filter = 'brightness(200%)'
+        setTimeout(() => {
+            colores[0].style.filter = 'brightness(100%)'
+        }, 1000)
     })
 
-    verde.addEventListener('click', () => { 
-        orden.push(verde)
-        console.log(orden)
+    colores[1].addEventListener('click', () => { 
+        orden.push(colores[1])
+        colores[1].style.filter = 'brightness(200%)'
+        setTimeout(() => {
+            colores[1].style.filter = 'brightness(100%)'
+        }, 1000)
     })
-    rojo.addEventListener('click', () => { 
-        orden.push(rojo)
-        console.log(orden)
+    colores[2].addEventListener('click', () => { 
+        orden.push(colores[2])
+        colores[2].style.filter = 'brightness(200%)'
+        setTimeout(() => {
+            colores[2].style.filter = 'brightness(100%)'
+        }, 1000)
     })
-    azul.addEventListener('click', () => { 
-        orden.push(azul)
-        console.log(orden)
+    colores[3].addEventListener('click', () => { 
+        orden.push(colores[3])
+        colores[3].style.filter = 'brightness(200%)'
+        setTimeout(() => {
+            colores[3].style.filter = 'brightness(100%)'
+            console.log(orden)
+            console.log(colores)
+            comparar_colors()
+        }, 1000)
     })
 }
 
 function comparar_colors() {
-    
-    if (colores == orden) {
-        alert('has ganado')
-    } else {
-        console.log('jijijija')
+
+    let verdadero = true
+
+   for (let i = 0; i < colores.length; i++) {
+        if (colores[i] !== orden[i]) {
+            alert('has perdido')
+            verdadero = false
+            break
+        }
+        
+        if ( i === colores.length -1) {
+            alert('has ganado')
+            break
+        }
+   
+    }   
+
+    if (verdadero == false) {
+        orden = []
+        console.log('arriba')
+        setTimeout(() => {
+            iluminar_carta()
+        }, 1000)
+    }
+
+    if (verdadero) {
+        siguiente_nivel()
     }
 }
 
 
+function siguiente_nivel() {
+    numeroNivel++
 
-
-
-
+}
