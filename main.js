ec0569b8856e08180ee95911f254031708cccaf2
Jordi <jordivallsplasmx@gmail.com>
2023-02-21 14:57:38 +0100

    Version 1.3

--- main.js
+++ main.js
@@ -1,115 +1,100 @@
-let colores = [amarillo, verde, rojo, azul];
+let colores = ['amarillo', 'verde', 'rojo', 'azul', 'morado', 'azul']
 let orden = []
-let numeroNivel = 1;
-console.log(colores)    
+let numeroNivel = 0
 
-    // colores[0].style.filter = 'brightness(200%)';    per a cambiar els colors 
-    // colores[1].style.filter = 'brightness(200%)';
-    // colores[2].style.filter = 'brightness(200%)';
-    // colores[3].style.filter = 'brightness(200%)';
 
 function jugar() {
+    boton_jugar.style.opacity = '0'
 
-    for (let i = 0; i < 1; i++) {
-        colores.sort( () => Math.random() - 0.5)
-    }
-    iluminar_carta()
-}
-
-function iluminar_carta() {
-    
-    colores[0].style.filter = 'brightness(200%)'
-
-    setTimeout(() => {
-        colores[0].style.filter = 'brightness(100%)'
-        colores[1].style.filter = 'brightness(200%)'
-    }, 1000);
-
-    setTimeout(() => {
-        colores[1].style.filter = 'brightness(100%)'
-        colores[2].style.filter = 'brightness(200%)'
-    }, 2000);
-
-    setTimeout(() => {
-        colores[2].style.filter = 'brightness(100%)'
-        colores[3].style.filter = 'brightness(200%)'
-    }, 3000);
-
-    setTimeout(() => {
-        colores[3].style.filter = 'brightness(100%)'
-        apretar_colors()
-    }, 4000)
-} 
-
-function apretar_colors() {
-    
-    colores[0].addEventListener('click', () => { 
-        orden.push(colores[0])
-        colores[0].style.filter = 'brightness(200%)'
-        setTimeout(() => {
-            colores[0].style.filter = 'brightness(100%)'
-        }, 1000)
-    })
-
-    colores[1].addEventListener('click', () => { 
-        orden.push(colores[1])
-        colores[1].style.filter = 'brightness(200%)'
-        setTimeout(() => {
-            colores[1].style.filter = 'brightness(100%)'
-        }, 1000)
-    })
-    colores[2].addEventListener('click', () => { 
-        orden.push(colores[2])
-        colores[2].style.filter = 'brightness(200%)'
-        setTimeout(() => {
-            colores[2].style.filter = 'brightness(100%)'
-        }, 1000)
-    })
-    colores[3].addEventListener('click', () => { 
-        orden.push(colores[3])
-        colores[3].style.filter = 'brightness(200%)'
-        setTimeout(() => {
-            colores[3].style.filter = 'brightness(100%)'
-            console.log(orden)
-            console.log(colores)
-            comparar_colors()
-        }, 1000)
-    })
-}
-
-function comparar_colors() {
-
-    let verdadero = true
-
-   for (let i = 0; i < colores.length; i++) {
-        if (colores[i] !== orden[i]) {
-            alert('has perdido')
-            verdadero = false
-            break
+    if (numeroNivel === 0) {
+        for (let i = 0; i < 4; i++) {
+            orden.push(desordenar_colores())
         }
-        
-        if ( i === colores.length -1) {
-            alert('has ganado')
+        numeroNivel++;
+    } else {
+        orden.push(desordenar_colores())
+        numeroNivel++
+    }
+    donar_colors()
+}
+
+function desordenar_colores() {
+
+    let random = Math.floor(Math.random() * 4 + 1)
+    
+    switch (random) {
+        case 1:
+            random = "amarillo"
             break
-        }
-   
-    }   
-
-    if (verdadero == false) {
-        orden = []
-        console.log('arriba')
-        setTimeout(() => {
-            iluminar_carta()
-        }, 1000)
+        case 2:
+            random = "verde"
+            break
+        case 3:
+            random = "rojo"
+            break
+        case 4:
+            random = "azul"
+            break
+        case 5:
+            random = "morado"
+            break
+        case 6:
+            random = "naranja"
+            break
     }
+    return random
+}
 
-    if (verdadero) {
-        siguiente_nivel()
+function donar_colors(num, color) {
+    switch(num || color) {
+        case "amarillo":
+            setTimeout(() => {
+                amarillo.style.filter =  "brightness(200%)"
+            }, 1000)
+            break
+        case "verde":
+            setTimeout(() => {
+                verde.style.filter =  "brightness(200%)"
+            }, 1000)
+            break
+        case "rojo":
+            setTimeout(() => {
+                rojo.style.filter =  "brightness(200%)"
+            }, 1000)
+            break
+        case "azul":
+            setTimeout(() => {
+                azul.style.filter =  "brightness(200%)"
+            }, 1000)
+            break
+        case "morado":
+            setTimeout(() => {
+                morado.style.filter =  "brightness(200%)"
+            }, 1000)
+            break
+        case "naranja":
+            setTimeout(() => {
+                naranja.style.filter =  "brightness(200%)"
+            }, 1000)
+            break
     }
 }
 
+function donarr_colors() {
+    let cont = 0
+    donarr_colors(orden[cont], "", 750)
+    cont++
 
-function siguiente_nivel() {
-    numeroNivel++
-
+    let interval = setInterval(() => {
+        donar_colors(orden[cont], "", 750)
+        cont++
+        if (cont == orden.length)
+        clearInterval(interval)
+        comparar()
+    }, 1000)
 }
+
+
+
+
+
